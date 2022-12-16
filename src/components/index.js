8241eee7275169483de2ca698479808aece9f245
Lindrit <lindritsulaj2.0@gmail.com>
2022-12-16 18:49:57 +0100

    ...

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Missing from "./Missing";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
+import Dropdown from "./Dropdown";
 
 export {
   Missing,
   Navbar,
   Footer,
-  Button
+  Button,
+  Dropdown
 }
