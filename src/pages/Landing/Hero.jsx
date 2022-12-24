12961fb949106245b05d268e5697eda109a4688f
Lindrit <lindritsulaj2.0@gmail.com>
2022-12-24 17:02:29 +0100

    Test

--- src/pages/Landing/Hero.jsx
+++ src/pages/Landing/Hero.jsx
@@ -6,6 +6,7 @@ const Hero = () => {
     <div className='hero-bg h-[calc(95vh-70px)]'>
       <section className="Hero max-w-[1600px] mx-auto flex flex-col justify-center h-full px-8">
         <div className='text max-w-[600px] mx-auto tablet:max-w-[800px] tablet:mx-0'>
+          <p>Hi</p>
           <h1 className='font-bold text-5xl text-neutral-900 text-center tablet:text-6xl tablet:text-left laptop:text-7xl font-playfair tablet:max-w-[600px] laptop:max-w-[none]'>We build your dream house</h1>
           <p className='mt-8 text-neutral-500 text-lg font-medium text-center tablet:text-left font-inter-tight max-w-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus libero voluptatum, sed inventore.</p>
           <div className='flex mt-8 gap-6 justify-center tablet:justify-start'>
