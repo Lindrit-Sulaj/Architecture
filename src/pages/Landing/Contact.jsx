fb3c7a5d37c9b8d10bfd226ceeb16e769cfd6295
Lindrit <lindritsulaj2.0@gmail.com>
2022-12-24 10:19:01 +0100

    ...

--- src/pages/Landing/Contact.jsx
+++ src/pages/Landing/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
           <p className='text-blue-200 my-8 font-noto-sans underline underline-offset-8 text-center tablet:text-left'>Monday - Friday : 7:30am - 4:00pm</p>
           <div className='flex mb-4 mt-14 items-center justify-center max-w-[none] gap-6 border-solid border-[2px] border-[transparent] transition-all cursor-pointer hover:border-blue-700 px-4 py-3 rounded-md tablet:max-w-[400px] tablet:justify-start'>
             <span className="material-symbols-outlined text-blue-400">call</span>
-            <span className='text-blue-300 text-lg font-inter-tight'>0425 804 026</span>
+            <span className='text-blue-300 text-lg font-inter-tight'>0000 000 000</span>
           </div>
           <div className='flex mb-4 items-center justify-center gap-6 border-solid border-[2px] border-[transparent] transition-all cursor-pointer hover:border-blue-700 px-4 py-3 rounded-md tablet:max-w-[400px] tablet:justify-start'>
             <span className="material-symbols-outlined text-blue-400">mail</span>
@@ -36,19 +36,19 @@ const Contact = () => {
         <form action="" className='w-full tablet:w-[40%] flex flex-col gap-y-8 py-7 px-5 bg-blue-750 border-solid border-[1px] border-blue-700 rounded-xl laptop:px-8'>
           <div className=''>
             <label htmlFor="name" className='text-blue-300 block w-full mb-2 font-noto-sans'>Name</label>
-            <input type="text" id='name' name='name' placeholder='John Doe' className='w-full block border-solid border-[1px] outline-none rounded-md border-blue-700 bg-[transparent] font-inter-tight px-4 py-2 focus:border-blue-300 focus:border-[2px] laptop:py-3' required/>
+            <input type="text" id='name' name='name' placeholder='John Doe' className='w-full block border-solid border-[1px] outline-none rounded-md border-blue-700 text-neutral-100 bg-[transparent] font-inter-tight px-4 py-2 focus:border-blue-300 focus:border-[2px] laptop:py-3' required/>
           </div>
           <div className=''>
             <label htmlFor="email" className='text-blue-300 block w-full mb-2 font-noto-sans'>Email</label>
-            <input type="email" name="email" id="email" placeholder='example@gmail.com' className='w-full block border-solid border-[1px] outline-none rounded-md border-blue-700 font-inter-tight px-4 py-2 bg-[transparent] focus:border-blue-300 focus:border-[2px] laptop:py-3' required />
+            <input type="email" name="email" id="email" placeholder='example@gmail.com' className='w-full block border-solid border-[1px] outline-none rounded-md border-blue-700 text-neutral-100 font-inter-tight px-4 py-2 bg-[transparent] focus:border-blue-300 focus:border-[2px] laptop:py-3' required />
           </div>
           <div className=''>
             <label htmlFor="phone" className='text-blue-300 block w-full mb-2 font-noto-sans'>Phone</label>
-            <input type="number" name="phone" id="phone" placeholder='+383' className='w-full block bg-[transparent] border-solid border-[1px] outline-none rounded-md border-blue-700 font-inter-tight px-4 py-2 focus:border-blue-300 focus:border-[2px] laptop:py-3' required />
+            <input type="text" name="phone" id="phone" placeholder='+383' className='w-full block bg-[transparent] border-solid border-[1px] outline-none text-neutral-100 rounded-md border-blue-700 font-inter-tight px-4 py-2 focus:border-blue-300 focus:border-[2px] laptop:py-3' required />
           </div>
           <div className=''>
             <label htmlFor="message" className='text-blue-300 block w-full mb-2 font-noto-sans'>Message</label>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder='Say something' className='w-full block border-solid border-[1px] outline-none bg-[transparent] rounded-md border-blue-700 font-inter-tight px-4 py-2 focus:border-blue-300 focus:border-[2px] resize-none h-[100px] laptop:py-3' required></textarea>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder='Say something' className='w-full block border-solid border-[1px] outline-none bg-[transparent] rounded-md border-blue-700 font-inter-tight px-4 py-2 focus:border-blue-300 focus:border-[2px] resize-none h-[100px] text-neutral-100 laptop:py-3' required></textarea>
           </div>
           <button type="submit" className='bg-blue-150 py-3 text-neutral-900 rounded-md font-semibold tracking-wide font-inter-tight focus:bg-blue-300'>Submit</button>
         </form>
